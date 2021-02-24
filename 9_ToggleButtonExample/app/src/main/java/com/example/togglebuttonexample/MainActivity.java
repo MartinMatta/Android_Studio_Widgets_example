2e2e3eb2d1b5dd244788850bdbb87131a49d9610
martinmatta355 <martinmatta355@gmail.com>
2021-02-24 17:05:53 +0100

    add 9_ToggleButtonExample

--- 9_ToggleButtonExample/app/src/main/java/com/example/togglebuttonexample/MainActivity.java
+++ 9_ToggleButtonExample/app/src/main/java/com/example/togglebuttonexample/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.togglebuttonexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton toggleButton;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toggleButton = findViewById(R.id.toggle);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(
+                        getBaseContext(),
+                        "button: " + toggleButton.getText(),
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
     }
 }
